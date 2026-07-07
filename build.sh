ab453d18d74bf83ee18c84ab4e994369aaf08470
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2026-07-07 09:25:37 +0700

    Comment out repo sync command
    Comment out the repo sync command in build.sh.

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ if [ -d .repo/local_manifests ] ;then
 else
 	git clone https://github.com/TrebleDroid/treble_manifest .repo/local_manifests -b $phh
 fi
-repo sync -c -j 1 --force-sync || repo sync -c -j1 --force-sync
+# repo sync -c -j 1 --force-sync || repo sync -c -j1 --force-sync
 
 (cd device/phh/treble; git clean -fdx; bash generate.sh)
 
