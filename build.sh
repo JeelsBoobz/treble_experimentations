91f1806c8629ba070de107ba8f52d98cab1c12e7
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2026-07-07 09:35:22 +0700

    Comment out generate script execution
    Comment out the command to generate files in the treble directory.

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ git clone https://github.com/TrebleDroid/treble_manifest .repo/local_manifests -
 
 # repo sync -c -j 1 --force-sync || repo sync -c -j1 --force-sync
 
-(cd device/phh/treble; git clean -fdx; bash generate.sh)
+# (cd device/phh/treble; git clean -fdx; bash generate.sh)
 
 . build/envsetup.sh
 
